03b9a29bd7c74713c46342ef6acac49d0e4ab958
Alisson Rayan <131839083+AlissonRayanss@users.noreply.github.com>
2024-05-02 15:37:35 -0300

    test: Update TesteUnitario.java

--- src/test/java/testes/TesteUnitario.java
+++ src/test/java/testes/TesteUnitario.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-public class GerenciarClientesTest {
+public class TesteUnitario {
 
     private GerenciarClientes gerenciador;
 
