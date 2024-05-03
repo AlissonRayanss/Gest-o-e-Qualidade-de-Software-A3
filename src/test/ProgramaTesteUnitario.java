ea941bb6f31a6e50ce2035b50ee65e88d2423fbd
WesleyccDantas <156453314+WesleyccDantas@users.noreply.github.com>
2024-05-03 15:08:56 -0300

    test: update ProgramaTesteUnitario.java

--- src/test/ProgramaTesteUnitario.java
+++ src/test/ProgramaTesteUnitario.java
@@ -14,7 +14,7 @@ import entidades.Cliente;
 import entidades.GerenciarClientes;
 
 @ExtendWith(MockitoExtension.class)
-public class GerenciarClientesTest {
+public class GerenciarClientes.java {
 
     private GerenciarClientes gerenciador;
 
