b5d0413d1f7c67dbf4b583789290a768b0209397
WesleyccDantas <156453314+WesleyccDantas@users.noreply.github.com>
2024-05-03 15:24:32 -0300

    test: update and rename TesteUnitario.java to GerenciarClientesTest.java

--- src/test/java/testes/GerenciarClientesTest.java
+++ src/test/java/testes/GerenciarClientesTest.java
@@ -7,8 +7,8 @@ import static org.mockito.Mockito.verify;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
 
 import entidades.Cliente;
 import entidades.GerenciarClientes;
